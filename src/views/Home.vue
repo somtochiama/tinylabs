39eef519bf433511b365c44fbe64a027f14be071
SomtochiAma <somtochionyekwere@gmail.com>
2019-10-01 18:06:26 +0100

    Adds dashboard and login links to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <router-link to="/dashboard">Dashboard</router-link>
-    <router-link to="/login">Login</router-link>
+    <router-link to="/dashboard">Dashboard &rarr; </router-link>
+    <router-link to="/login">Login &rarr;</router-link>
   </div>
 </template>
 
@@ -16,3 +16,17 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+
+  .home {
+    a {
+      text-decoration: none;
+      color: #2BDA53;
+      display: inline-block;
+      padding: 1rem;
+      margin: 1rem;
+    }
+  }
+  
+</style>

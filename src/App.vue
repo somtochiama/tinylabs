1e6b863708690b997cd0c462a060b1cf1377c1d1
SomtochiAma <somtochionyekwere@gmail.com>
2019-10-02 12:59:02 +0100

    Styles links

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<style lang="scss">
-
-// #nav {
-//   padding: 30px;
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="home">
-    <router-link to="/dashboard">Dashboard</router-link>
-    <router-link to="/login">Login</router-link>
+    <router-link to="/dashboard">Dashboard &rarr;</router-link>
+    <router-link to="/login">Login &rarr;</router-link>
   </div>
 </template>
+
+<style lang="scss">
+  .home {
+    a {
+      color: #2bda53;
+      text-decoration: none;
+      padding: 10px;
+      margin: 10px;
+    }
+  }
+</style>
 
 <script>
 // @ is an alias to /src
